88be372157e715f207bfd6ea1cac1c0a19b0d9e8
minhnd-2550 <85108779+minhnd-2550@users.noreply.github.com>
2022-09-23 00:38:39 +0700

    add fe

---
+++ chat_app_fe/src/App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { LoginPage } from './pages/LoginPage';
+import { RegisterPage } from './pages/RegisterPage';
+import { ConversationChannelPage } from './pages/ConversationChannelPage';
+import { ConversationPage } from './pages/ConversationPage';
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="conversations" element={<ConversationPage />}>
+          <Route path=":id" element={<ConversationChannelPage />} />
+        </Route>
+      </Routes>
+    </>
+  );
+}
+
+export default App;
---
+++ chat_app_fe/src/components/forms/RegisterForm.tsx
@@ -0,0 +1,70 @@
+import { Link } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+import {
+    Button,
+    InputContainer,
+    InputField,
+    InputLabel,
+} from '../../utils/styles';
+import styles from './index.module.scss';
+
+export const RegisterForm = () => {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm();
+
+    console.log(errors);
+    const onSubmit = (data: any) => {
+        console.log(data);
+    };
+
+    return (
+        <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
+            <InputContainer>
+                <InputLabel htmlFor="email">Email</InputLabel>
+                <InputField
+                    type="email"
+                    id="email"
+                    {...register('email', {
+                        required: 'Email is required',
+                    })}
+                />
+            </InputContainer>
+            <section className={styles.nameFieldRow}>
+                <InputContainer>
+                    <InputLabel htmlFor="firstName">First Name</InputLabel>
+                    <InputField
+                        type="text"
+                        id="firstName"
+                        {...register('firstName', { required: 'First Name is Required' })}
+                    />
+                </InputContainer>
+                <InputContainer>
+                    <InputLabel htmlFor="lastName">Last Name</InputLabel>
+                    <InputField
+                        type="text"
+                        id="lastName"
+                        {...register('lastName', { required: 'Last Name is Required' })}
+                    />
+                </InputContainer>
+            </section>
+            <InputContainer>
+                <InputLabel htmlFor="password">Password</InputLabel>
+                <InputField
+                    type="password"
+                    id="password"
+                    {...register('password', { required: 'Password is Required' })}
+                />
+            </InputContainer>
+            <Button className={styles.button}>Create My Account</Button>
+            <div className={styles.footerText}>
+                <span>Already have an account? </span>
+                <Link to="/login">
+                    <span>Login</span>
+                </Link>
+            </div>
+        </form>
+    );
+};
